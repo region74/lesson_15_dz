a7d39d2f5ea904b1d1cec82a50c6caeada878f3e
Кирилл <kiryamiass7@gmail.com>
2022-12-07 16:22:50 +0500

    change is_num_to_cart and try to change is_empty functions

--- tmp.py
+++ tmp.py
@@ -1,17 +1,27 @@
 import random
 
-a = [1, 3, 4, 5, 6, 7, '3', '#', '#', '#']
-# random.shuffle(a)
-print(a)
-c = 3
-b = a.index(c)
-print(b)
+aa = ['#', '#', 1]
 
-count = 0
-for item in a:
-    if str(item).isdigit():
-        count += 1
 
-print(count)
-b=a[0:2]+a[2:4]
-print(b)
+# # random.shuffle(a)
+# print(a)
+# c = 3
+# b = a.index(c)
+# print(b)
+#
+# count = 0
+# for item in a:
+#     if str(item).isdigit():
+#         count += 1
+#
+# print(count)
+# b=a[0:2]+a[2:4]
+# print(b)
+
+
+def is_empty():
+    f = lambda aa: True if str.isdigit() in aa else False
+    return f
+
+
+print(is_empty())
